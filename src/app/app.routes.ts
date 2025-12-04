4eb0bda632c07b7fbfa5e38498cc02ddbdc38ba6
fernandomartinez-neoland <fernando.martinez@neoland.es>
2025-12-04 11:56:12 +0100

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RutaComponent } from './ruta/ruta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'ruta',
+        component: RutaComponent
+    }
+];
