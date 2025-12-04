83b6cb23a97f20f2c5592ce3a6d24a7cdd3d2413
fernandomartinez-neoland <fernando.martinez@neoland.es>
2025-12-04 12:43:05 +0100

    router link

--- src/app/subruta/subruta.component.ts
+++ src/app/subruta/subruta.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-subruta',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './subruta.component.html',
   styleUrl: './subruta.component.css',
 })
